bbaadabffc0f983bcd0eff92bde8b881ea159296
Girik soni <giriksoni0710@gmail.com>
2019-08-25 04:08:41 -0700

    1st version design

--- src/app/bookservice/bookservice-component.component.ts
+++ src/app/bookservice/bookservice-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-bookservice-component',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookserviceComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  sendEmail(){
+
+   alert("hello");
+ 
   }
 
 }
